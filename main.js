9a448c182da4c21b70e879b2f8b712e2fd1f1a74
sankaralingam <125450819+msankaralingam@users.noreply.github.com>
2023-06-16 16:03:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ while(a>0){
 	 b=(b*10)+rem;
 }
 console.log(b);
+
+2.part 2
+
+
+
+var a=4561;
+	var c=0;
+	var z=0;
+	
+	
+	while(a!=0){
+		
+		var b=a%10;
+		a=(a-b)/10;
+		if(b%2==0){
+			
+			c=c+b;
+		}
+	}
+	
+	console.log(c);
